f4f75654dca3a68ef68a5cba5b2ccb0c8c5fb726
Juan Luna <juandluna17@gmail.com>
2022-09-25 21:03:52 -0500

    :closed_lock_with_key: Configurar Webhook WhatsApp para verificación de token

--- src/controllers/whatsappController.js
+++ src/controllers/whatsappController.js
@@ -1,5 +1,17 @@
 const VerifyToken = (req,res)=>{
-    res.send("hola verifyToken");
+    try {
+        var accessToken = "WT4zxPRNQgkYv6IQ9bEy";
+        var token = req.query["hub.verify_token"]
+        var challenge = req.query["hub.challenge"];
+
+        if (challenge != null && token != null && token === accessToken){
+            res.send(challenge);
+        }else{
+            res.status(400).send();
+        }
+    } catch (e) {
+        res.status(400).send();
+    }
 }
 
 const ReceivedMessage = (req,res)=>{
